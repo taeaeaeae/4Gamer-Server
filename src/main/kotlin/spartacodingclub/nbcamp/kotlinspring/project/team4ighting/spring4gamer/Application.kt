b34115fcbb06db68f7d9e7ae4abfd8e0728854d3
Turbstructor <ycw0201@naver.com>
2024-07-16 14:22:34 +0900

    🎉 Initial commit

---
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/Application.kt
@@ -0,0 +1,11 @@
+package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
+}
---
+++ src/test/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
