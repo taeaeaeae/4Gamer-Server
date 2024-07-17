4e2467ff96888bcf7ca4cddf8446967a1f91db9e
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-17 14:45:08 +0900

    :adhesive_bandage: Fix: GET 만 모두 허용으로 변경할 때 생긴 중복 제거

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
@@ -30,8 +30,7 @@ class SecurityConfig(
                     .permitAll()
                 it.requestMatchers(
                     "/api/v1/auth/signin",
-                    "/api/v1/auth/signup",
-                    "/api/v1/members/{id}"
+                    "/api/v1/auth/signup"
                 )
                     .permitAll()
                     .anyRequest().authenticated()
