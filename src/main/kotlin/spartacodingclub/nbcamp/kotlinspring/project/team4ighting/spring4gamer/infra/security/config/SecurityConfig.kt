162886aef942058554f66a07da84f2bed9694e1c
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-17 12:29:00 +0900

    :adhesive_bandage: Fix: 모든 API 인증 허용(임시)

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
@@ -29,6 +29,7 @@ class SecurityConfig(
                     "/api/v1/auth/signin",
                     "/api/v1/auth/signup",
                     "/api/v1/members/{id}",
+                    "/**"
                 )
                     .permitAll()
                     .anyRequest().authenticated()
