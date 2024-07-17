429e9db9aa490e2ae352676327ebffff0e595fb5
eunhye han <hellou8363@gmail.com>
2024-07-17 13:14:27 +0900

    Feat: 댓글 수정

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/controller/CommentController.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/controller/CommentController.kt
@@ -1,5 +1,6 @@
 package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.controller
 
+import jakarta.validation.Valid
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Sort
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.CommentResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.CreateCommentRequest
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.UpdateCommentRequest
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.service.CommentService
 
 @RestController
@@ -23,7 +25,7 @@ class CommentController(
         @PathVariable("channelId") channelId: Long,
         @PathVariable("boardId") boardId: Long,
         @PathVariable("postId") postId: Long,
-        @RequestBody request: CreateCommentRequest
+        @RequestBody @Valid request: CreateCommentRequest
     ): ResponseEntity<CommentResponse> {
 
         // TODO: 로그인 구현 후 임시 유저 ID인 1L 제거
@@ -44,6 +46,21 @@ class CommentController(
             .status(HttpStatus.OK)
             .body(commentService.getCommentList(channelId, boardId, postId, pageable))
     }
-    // TODO: 수정
+
+    @PutMapping("/{commentId}")
+    fun updateComment(
+//        @AuthenticationPrincipal memberId: Long, // TODO: 로그인 구현 후 사용 예정
+        @PathVariable("channelId") channelId: Long,
+        @PathVariable("boardId") boardId: Long,
+        @PathVariable("postId") postId: Long,
+        @PathVariable("commentId") commentId: Long,
+        @RequestBody @Valid request: UpdateCommentRequest
+    ): ResponseEntity<CommentResponse> {
+
+        // TODO: 로그인 구현 후 임시 유저 ID인 1L 제거
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(commentService.updateComment(channelId, boardId, postId, commentId, request, 2L))
+    }
     // TODO: 삭제
 }
--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/service/CommentService.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/service/CommentService.kt
@@ -2,14 +2,17 @@ package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.d
 
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.CommentResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.CreateCommentRequest
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.dto.UpdateCommentRequest
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.model.Comment
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.model.toResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.repository.CommentRepository
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.post.repository.PostRepository
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.exception.CustomAccessDeniedException
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.exception.ModelNotFoundException
 
 @Service
@@ -57,5 +60,31 @@ class CommentService(
         val comment = commentRepository.findByPostId(postId, pageable)
 
         return comment.map { it.toResponse() }
+    }
+
+    @Transactional
+    fun updateComment(
+        channelId: Long,
+        boardId: Long,
+        postId: Long,
+        commentId: Long,
+        request: UpdateCommentRequest,
+        memberId: Long
+    ): CommentResponse {
+
+        // TODO: board 구현 후 사용 예정
+//        val board = commentRepository.findByIdAndChannel(boardId, channelId) ?: throw ModelNotFoundException("Board", boardId)
+        val post = postRepository.findByIdAndBoard(postId, boardId) ?: throw ModelNotFoundException("Post", postId)
+        val comment = commentRepository.findByIdAndPostId(commentId, postId) ?: throw ModelNotFoundException("Comment", commentId)
+
+        if (comment.memberId != memberId) {
+            throw CustomAccessDeniedException("해당 댓글에 대한 수정 권한이 없습니다.")
+        }
+
+        comment.update(
+            content = request.content
+        )
+
+        return comment.toResponse()
     }
 }
