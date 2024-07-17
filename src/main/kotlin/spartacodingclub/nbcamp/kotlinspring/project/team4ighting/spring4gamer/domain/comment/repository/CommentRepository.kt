386103068847ab1a50dd46861ffc7c1d669ac779
eunhye han <hellou8363@gmail.com>
2024-07-18 00:02:04 +0900

    Fix: 게시글 삭제 시 관련 댓글도 삭제 되도록 수정

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/comment/repository/CommentRepository.kt
@@ -4,11 +4,12 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.comment.model.Comment
-import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.post.model.Post
 
 interface CommentRepository : JpaRepository<Comment, Long> {
 
     fun findByPostId(postId: Long, pageable: Pageable): Page<Comment>
 
     fun findByIdAndPostId(commentId: Long, postId: Long): Comment?
+
+    fun deleteByPostId(postId: Long)
 }
