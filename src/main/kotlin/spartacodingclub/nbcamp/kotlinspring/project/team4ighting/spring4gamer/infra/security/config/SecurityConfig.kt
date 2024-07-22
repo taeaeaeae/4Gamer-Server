8b33dcf32c2a0da8d88de786894ce74f360d2120
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-22 10:54:43 +0900

    :heavy_plus_sign: Dependency: oauth2 의존성 추가

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/infra/security/config/SecurityConfig.kt
@@ -1,28 +1,32 @@
 package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.infra.security.config
 
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.infra.security.CustomAuthenticationEntryPoint
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.infra.OAuth2Service
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.infra.security.jwt.jwtAuthenticationFilter
+
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig(
     private val jwtAuthenticationFilter: jwtAuthenticationFilter,
-    private val authenticationEntryPoint: CustomAuthenticationEntryPoint,
+    private val authenticationEntryPoint: AuthenticationEntryPoint,
+    private val customOAuth2UserService: OAuth2Service,
 ) {
 
     @Bean
-    fun filterChain(http: HttpSecurity): SecurityFilterChain =
-
-        http
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
             .csrf { it.disable() }
@@ -36,9 +40,23 @@ class SecurityConfig(
                     .permitAll()
                     .anyRequest().authenticated()
             }
+            .sessionManagement {
+                it.sessionCreationPolicy(SessionCreationPolicy.NEVER)
+            }
+            .oauth2Login {
+                it.loginProcessingUrl("/google")
+                it.userInfoEndpoint { u -> u.userService(customOAuth2UserService) }
+                it.defaultSuccessUrl("/api/v1/auth/signin/google")
+                it.failureHandler { request, response, exception ->
+                    response.status = HttpServletResponse.SC_UNAUTHORIZED
+                    response.contentType = "application/json"
+                    response.writer.write("{\"error\":\"${exception.message}\"}")
+                }
+            }
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter::class.java)
             .exceptionHandling {
                 it.authenticationEntryPoint(authenticationEntryPoint)
             }
             .build()
+    }
 }
