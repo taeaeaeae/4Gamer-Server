9af493ccbcca6cb188fcd683f354cdd1b77c1932
LeeTaekyoung <46617216+taeaeaeae@users.noreply.github.com>
2024-07-22 11:28:54 +0900

    :atr: Style: 리포맷팅

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/dto/response/OAuth2Response.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/dto/response/OAuth2Response.kt
@@ -1,15 +1,13 @@
 package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.dto.response
 
-class OAuth2Response(private val attribute: Map<String, Any>) {
-    val provider: String
-        get() = "google"
+class OAuth2Response(
+    private val attribute: Map<String, Any>
+) {
+    val provider = "google"
 
-    val providerId: String
-        get() = attribute["sub"].toString()
+    val providerId = attribute["sub"].toString()
 
-    val email: String
-        get() = attribute["email"].toString()
+    val email = attribute["email"].toString()
 
-    val name: String
-        get() = attribute["name"].toString()
+    val name = attribute["name"].toString()
 }
