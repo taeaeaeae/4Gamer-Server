f696aa9cba02b8dcbfbc7f9b1f4060db473dbaac
Turbstructor <ycw0201@naver.com>
2024-07-22 16:35:44 +0900

    ✨ Feat: Implement `MemberBlacklist`
    This commit introduces new feature "blacklisting member", in which one `Member` can blacklist another `Member`. Basic create/delete oepration has been implemented, while features using `MemberBlacklist` will be implemented in later commits.

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/controller/MemberController.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/controller/MemberController.kt
@@ -3,9 +3,12 @@ package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.d
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.dto.response.MemberResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.dto.response.MemberSimplifiedResponse
@@ -33,4 +36,26 @@ class MemberController(
         ResponseEntity
             .status(HttpStatus.OK)
             .body(memberService.getSimpleMember(UUID.fromString(id)))
+
+
+    @PostMapping("/member/blacklist")
+    fun addBlacklist(
+        @AuthenticationPrincipal principal: MemberPrincipal,
+        @RequestParam("target-id") targetId: UUID
+    ): ResponseEntity<Unit> =
+
+        ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(memberService.addBlacklist(principal.id, targetId))
+
+
+    @DeleteMapping("/member/blacklist")
+    fun removeBlacklist(
+        @AuthenticationPrincipal principal: MemberPrincipal,
+        @RequestParam("target-id") targetId: UUID
+    ): ResponseEntity<Unit> =
+
+        ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .body(memberService.removeBlacklist(principal.id, targetId))
 }
--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/service/MemberService.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/domain/member/service/MemberService.kt
@@ -5,15 +5,18 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.dto.response.MemberResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.dto.response.MemberSimplifiedResponse
-import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.model.Member
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.model.MemberBlacklist
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.model.toResponse
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.model.toSimplifiedResponse
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.repository.MemberBlacklistRepository
 import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.domain.member.repository.MemberRepository
+import spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.exception.ModelNotFoundException
 import java.util.UUID
 
 @Service
 class MemberService(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val memberBlacklistRepository: MemberBlacklistRepository
 ) {
 
     fun getMember(id: UUID): MemberResponse =
@@ -28,4 +31,38 @@ class MemberService(
         memberRepository.findByIdOrNull(id)
             ?.toSimplifiedResponse()
             ?: throw EntityNotFoundException("model not found")
+
+
+    fun addBlacklist(
+        memberId: UUID,
+        targetId: UUID
+    ) {
+
+        val member = memberRepository.findByIdOrNull(memberId)
+            ?: throw ModelNotFoundException("Member", memberId)
+        val target = memberRepository.findByIdOrNull(targetId)
+            ?: throw ModelNotFoundException("Member", targetId)
+
+        if (memberBlacklistRepository.existsByIdSubjectIdAndIdTargetId(memberId, targetId))
+            throw IllegalArgumentException("Blacklist already added")
+
+        memberBlacklistRepository.save(
+            MemberBlacklist.from(
+                subject = member,
+                target = target
+            )
+        )
+    }
+
+
+    fun removeBlacklist(
+        memberId: UUID,
+        targetId: UUID
+    ) {
+
+        val targetMemberBlacklist = memberBlacklistRepository.findByIdSubjectIdAndIdTargetId(memberId, targetId)
+            ?: throw ModelNotFoundException("MemberBlacklist", "${memberId}/${targetId}")
+
+        memberBlacklistRepository.delete(targetMemberBlacklist)
+    }
 }
