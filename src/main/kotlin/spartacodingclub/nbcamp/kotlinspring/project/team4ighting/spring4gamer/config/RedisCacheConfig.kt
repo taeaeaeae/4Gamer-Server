82a80b1c6a5afbf3c746e52825f04a98b803e517
eunhye han <hellou8363@gmail.com>
2024-07-23 12:21:07 +0900

    Fix: Redis 내 데이터 직렬/역직렬화 적용

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/config/RedisCacheConfig.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/config/RedisCacheConfig.kt
@@ -1,5 +1,7 @@
 package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.config
 
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
@@ -7,6 +9,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
 
 @Configuration
@@ -24,6 +29,21 @@ class RedisCacheConfig {
                     .prefixCacheNameWith(this.javaClass.packageName + ".")
                     .entryTtl(Duration.ofMinutes(1))
                     .disableCachingNullValues()
+                    .serializeKeysWith(
+                        RedisSerializationContext
+                            .SerializationPair
+                            .fromSerializer(StringRedisSerializer())
+                    )
+                    .serializeValuesWith(
+                        RedisSerializationContext
+                            .SerializationPair
+                            .fromSerializer(
+                                GenericJackson2JsonRedisSerializer(
+                                    jacksonObjectMapper()
+                                        .registerModules(JavaTimeModule())
+                                )
+                            )
+                    )
             )
             .build()
 
