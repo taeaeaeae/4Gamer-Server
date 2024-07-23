b2da79ca795a53994fc7a9d67218ae099b55242b
eunhye han <hellou8363@gmail.com>
2024-07-23 14:32:38 +0900

    Fix: Redis 내 데이터 역직렬화 시 발생하는 문제 해결

--- src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/config/RedisCacheConfig.kt
+++ src/main/kotlin/spartacodingclub/nbcamp/kotlinspring/project/team4ighting/spring4gamer/config/RedisCacheConfig.kt
@@ -1,5 +1,7 @@
 package spartacodingclub.nbcamp.kotlinspring.project.team4ighting.spring4gamer.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.jsontype.BasicPolymorphicTypeValidator
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.cache.CacheManager
@@ -26,7 +28,7 @@ class RedisCacheConfig {
             .cacheDefaults(
                 RedisCacheConfiguration
                     .defaultCacheConfig()
-                    .prefixCacheNameWith(this.javaClass.packageName + ".")
+//                    .prefixCacheNameWith(this.javaClass.simpleName + ".")
                     .entryTtl(Duration.ofMinutes(1))
                     .disableCachingNullValues()
                     .serializeKeysWith(
@@ -41,6 +43,13 @@ class RedisCacheConfig {
                                 GenericJackson2JsonRedisSerializer(
                                     jacksonObjectMapper()
                                         .registerModules(JavaTimeModule())
+                                        .activateDefaultTyping(
+                                            BasicPolymorphicTypeValidator
+                                                .builder()
+                                                .allowIfSubType(Object::class.java)
+                                                .build(),
+                                            ObjectMapper.DefaultTyping.NON_FINAL
+                                        )
                                 )
                             )
                     )
